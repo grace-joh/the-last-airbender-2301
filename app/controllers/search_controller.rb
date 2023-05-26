507862499faadef8ca77e301cd2c599a0af223cf
Grace Joh <graceehjoh@gmail.com>
2023-05-26 15:58:04 -0500

    Refactor API consumption out of index action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,6 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get("https://last-airbender-api.fly.dev/api/v1/characters?affiliation=#{search_params[:nation]}&perPage=500")
-    json = JSON.parse(response.body, symbolize_names: true)
-    # binding.pry
-    @members = json.map do |member|
-      Member.new(member)
-    end
+    @members = AvatarFacade.new.members_by_nation(search_params[:nation])
   end
 
   private
